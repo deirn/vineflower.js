7a257563c0352acf232313612146eae82b5919bf
Dimas Firmansyah <deirn@bai.lol>
2026-02-06 20:37:01 +0700

    Updates

--- src/org/jetbrains/java/decompiler/modules/decompiler/flow/DirectEdgeType.java
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.java.decompiler.modules.decompiler.flow;
-
-public enum DirectEdgeType {
-  REGULAR,
-  EXCEPTION;
-
-  // read-only!
-  public static final DirectEdgeType[] TYPES = values();
-}
